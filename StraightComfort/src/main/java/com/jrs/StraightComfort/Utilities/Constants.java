fea36a036e3b524fc1c40f3d14c339bbb152d0c1
JMtorii <j.torii4@gmail.com>
2014-03-19 12:42:44 -0400

    Added splash screen

---
+++ StraightComfort/src/main/java/com/jrs/StraightComfort/Utilities/Constants.java
@@ -0,0 +1,8 @@
+package com.jrs.StraightComfort.Utilities;
+
+/**
+ * Created by JMtorii on 2014-03-19.
+ */
+public class Constants {
+    public static final String TEST_CONSTANT = "ALWAYS USE THIS FORMAT";
+}
---
+++ StraightComfort/src/main/java/com/jrs/StraightComfort/Views/SplashScreenActivity.java
@@ -0,0 +1,41 @@
+package com.jrs.StraightComfort.Views;
+
+/**
+ * Created by JMtorii on 2014-03-19.
+ */
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.jrs.StraightComfort.R;
+
+public class SplashScreenActivity extends Activity {
+    private static final int TIME = 4 * 1000;// 4 seconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+                SplashScreenActivity.this.finish();
+                overridePendingTransition(R.anim.splash_fade_in, R.anim.splash_fade_out);
+            } }, TIME);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override public void run() {
+
+            } }, TIME);
+    }
+
+    @Override public void onBackPressed() {
+        this.finish();
+        super.onBackPressed();
+    }
+}
